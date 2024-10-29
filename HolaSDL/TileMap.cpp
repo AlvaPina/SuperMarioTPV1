db5d151bca970452b77e933e2abf71fc260c5dbc
Alvaro <alvapina@ucm.es>
2024-10-29 17:28:18 +0100

    a

--- HolaSDL/TileMap.cpp
+++ HolaSDL/TileMap.cpp
@@ -1,1 +1,11 @@
 #include "TileMap.h"
+
+#include <iostream>
+
+TileMap::TileMap(Game* game, Texture* patternsTexture, int indices[16][32])
+	: _game(game), _patternsTexture(patternsTexture)
+{
+	for (int i = 0; i < sizeof(indices[0]); i++) {
+
+	}
+}
--- HolaSDL/TileMap.h
+++ HolaSDL/TileMap.h
@@ -1,7 +1,18 @@
 #pragma once
+
+#include "Game.h"
+
 class TileMap
 {
 private:
 	static const int TILE_MAP = 2;
+
+	int _indices[16][32];
+	Game* _game;
+	Texture* _patternsTexture;
+
+public:
+	TileMap(Game* game, Texture* patternsTexture, int indices[16][32]);
+
 };
 
