e0515d4b060e04749f244ab066548a07e20db4e1
Mauro Martínez <maumar03@ucm.es>
2024-11-11 13:46:12 +0100

    MarioBase

--- HolaSDL/Player.cpp
+++ HolaSDL/Player.cpp
@@ -1,1 +1,37 @@
 #include "Player.h"
+#include "Vector2D.h"
+#include "Texture.h"
+#include "SDL.h"
+#include "Game.h"
+
+
+Player::Player(SDL_Texture* text, Vector2D<int> pos, Game* gam, int livs, bool movR, bool supMar):
+	texture(text), position(pos), game(gam), lives(livs), movingRight(movR), SuperMario(supMar)
+{
+
+
+}
+
+void Player::render()
+{
+
+
+
+}
+
+void Player::update()
+{
+
+
+}
+
+void Player::hit()
+{
+	if (SuperMario) SuperMario = false;
+	else lives--; // ha muerto
+}
+
+void Player::handleEvent()
+{
+	
+}
--- HolaSDL/Player.h
+++ HolaSDL/Player.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "Vector2D.h"
+#include "Texture.h"
+#include "SDL.h"
+#include "Game.h"
+
 class Player
 {
+private:
+	SDL_Texture* texture;	// Puntero a la textura de Mario
+	Vector2D<int> position;	// Posicion actual de Mario
+	Game* game;				// Puntero al juego
+	int lives;				// Numero de vidas de Mario
+	bool movingRight;			// Direccion de Mario
+	bool SuperMario;		// Forma de Mario
+
+public:
+	void render();
+	void update();
+	void hit();
+	void handleEvent();
+
+	Player(SDL_Texture* text, Vector2D<int> pos, Game* gam, int livs, bool movR, bool supMar);
+	~Player();
 };
 
