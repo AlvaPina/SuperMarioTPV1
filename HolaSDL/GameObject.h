0af23384b64ca1ccb4d7585463a7378ce71cab0b
Alvaro <alvapina@ucm.es>
2024-11-21 17:49:49 +0100

    GameObject

---
+++ HolaSDL/GameObject.h
@@ -0,0 +1,21 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "checkML.h"
+
+class Game;
+
+class GameObject
+{
+protected:
+    Game* game;
+public:
+    GameObject(Game* game);
+    virtual ~GameObject();
+
+    // metodos virtuales
+    virtual void Render() const = 0;
+    virtual void Update() = 0;
+};
+#endif
+
